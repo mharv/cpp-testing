d16c2fb590c147c9babb162da68c2a1192be4941
mharv <mitch.k.harvey@gmail.com>
2023-12-17 14:10:58 +0000

    playin

--- main.cpp
+++ main.cpp
@@ -1,37 +1,47 @@
 #include <iostream>
-// import setprecision
-#include <iomanip>
+#include <string>
+#include <vector>
+// print function that takes a generic type and prints it
 
-int main ()
+using namespace std;
+template <typename T>
+
+void print(T value)
 {
-    // initialize a number variable
-    int number = 0;
-    // initialize a sum variable
-    int sum = 0;
-    // initialize an array of numbers with random values
-    int numbers[5] = {1, 2, 3, 4, 5};
-    //loop through the numbers array and multiply each number by 7.5 then add it to the sum variable
-    for (int i = 0; i < 5; i++)
+    cout << value << "\n";
+}
+
+int main()
+{
+    int a = 4;
+    double b = 3.0;
+    char c[] = "hello";
+    short test = 0b010101; // 21
+    short test1 = 123123;
+    cout << a << " " << b << " " << c << "\n";
+    cout << test << "\n";
+    cout << test1 << "\n";
+
+    auto somethingElse = 9;
+    print(test1);
+    print(somethingElse);
+    // print the type of somethingElse
+    cout << typeid(somethingElse).name() << "\n";
+    std::string s = "hello";
+    std::string types = typeid(s).name();
+
+    std::vector<char> empty_dynamic_array;
+    for (int i = 0; i < types.length(); i++)
     {
-        sum += numbers[i] * 7.5;
+        // index the string and push back the character
+        empty_dynamic_array.push_back(types.at(i));
     }
-    // print the sum variable
-    std::cout << sum << std::endl;
-    // print the sum variable divided by 5
-    std::cout << sum / 5 << std::endl;
-    // print the sum variable divided by 5.0
-    std::cout << sum / 5.0 << std::endl;
-    // print the sum variable divided by 5.0 with 2 decimal places
-    std::cout << std::fixed << std::setprecision(2) << sum / 5.0 << std::endl;
-    // print the sum variable divided by 5.0 with 2 decimal places and a dollar sign
-    std::cout << "$" << std::fixed << std::setprecision(2) << sum / 5.0 << std::endl;
-    // print the sum variable divided by 5.0 with 2 decimal places and a dollar sign and a space
-    std::cout << "$ " << std::fixed << std::setprecision(2) << sum / 5.0 << std::endl;
-    // print the sum variable divided by 5.0 with 2 decimal places and a dollar sign and a space and a 0
-    std::cout << "$ " << std::fixed << std::setprecision(2) << sum / 5.0 << "0" << std::endl;
 
-    
-
+    // print out the array on one line
+    for (int i = 0; i < empty_dynamic_array.size(); i++)
+    {
+        cout << empty_dynamic_array[i] << " ";
+    }
 
     return 0;
 }
